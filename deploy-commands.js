7bbc77ca66d89b79e5f9ca055459207ddf0b6100
Rohit Bhetal <96344296+Rohit-Bhetal@users.noreply.github.com>
2024-07-18 11:56:54 +0545

    deploy-commands and slash commands

---
+++ deploy-commands.js
@@ -0,0 +1,40 @@
+const {REST,Routes} = require('discord.js');
+const fs= require('fs');
+const path= require('path');
+require('dotenv').config;
+
+const commands =[];
+const foldersPath = path.join(__dirname,'commands');
+const commandFolders = fs.readdirSync(foldersPath);
+for(const folder of commandFolders){
+  const commandsPath = path.join(foldersPath,folder);
+  const commonFiles = fs.readdirSync(commandsPath).filter(file=>file.endsWith('.js'));
+  for (const file of commonFiles){
+    const filePath = path.join(commandsPath,file);
+    const command = require(filePath);
+    if('data' in command && 'execute' in command){
+      commands.push(command.data.toJSON());
+    }else{
+      console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`)
+    }
+  }
+}
+//Contruct and preparing an instance of the rest module
+const rest = new REST().setToken(process.env.TOKEN);
+
+//deploy your command
+(async()=>{
+    try{
+        console.log(`Started refreshing ${commands.length}`);
+
+        const data =  await rest.put(
+            Routes.applicationGuildCommands(process.env.CLIENT_ID,process.env.GUILDID),
+            {
+                body:commands
+            }
+        );
+        console.log(`Successfully reloaded ${data.length} application (/) commands.`)
+    }catch(err){
+        console.error(err);
+    }
+})();
--- index.js
+++ index.js
@@ -1,16 +1,45 @@
 //Require the necessary discord,js classes
-const {Client,Events,GatewayIntentsBits} = require('discord.js');
-const {token} = require('./config.json');
-
+const {Client,Events,GatewayIntentsBits, GatewayIntentBits, Collection, ClientApplication} = require('discord.js');
+require('dotenv').config;
+const fs = require('fs')
+const path = require('path');
+const { CLIENT_RENEG_WINDOW } = require('tls');
 // Create a new Client instance
 const client = new Client({
   intents:[
-    GatewayIntentsBits.Guilds
+    GatewayIntentBits.Guilds
   ]
 });
-
-client.once(
-  Events.ClientReady,readyClient =>{
-    console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+client.commands = new Collection();
+const foldersPath = path.join(__dirname,'commands');
+const commandFolders = fs.readdirSync(foldersPath);
+for(const folder of commandFolders){
+  const commandsPath = path.join(foldersPath,folder);
+  const commonFiles = fs.readdirSync(commandsPath).filter(file=>file.endsWith('.js'));
+  for (const file of commonFiles){
+    const filePath = path.join(commandsPath,file);
+    const command = require(filePath);
+    if('data' in command && 'execute' in command){
+      client.commands.set(command.data.name,command);
+    }else{
+      console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`)
+    }
   }
-)
+}
+
+const eventsPath = path.join(__dirname,'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(files=>files.endsWith('.js'));
+
+for(const file of eventFiles){
+  const filePath =path.join(eventsPath,file);
+  const event = require(filePath);
+  if(event.once){
+    client.once(event.name,(...args)=>event.execute(...args));
+  }else{
+    client.on(event.name,(...args)=> event.execute(...args))
+  }
+}
+
+
+
+client.login(process.env.TOKEN);
